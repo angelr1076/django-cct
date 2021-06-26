287dbd56b1661dfac3f5917e3cf90f2d8af17197
Angel Rodriguez <angelr1076@gmail.com>
2021-06-25 20:58:34 -0400

    Updated STATIC_ROOT

--- casecrimetrace/settings.py
+++ casecrimetrace/settings.py
@@ -138,11 +138,11 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'casecrimetrace/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'casecrimetrace/static')
+# ]
 
 # Media folder settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
