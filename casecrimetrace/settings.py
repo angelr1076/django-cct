2d4fc203f123f1cb33431b32aca6415c1561cf4d
angelr1076 <angelr1076@gmail.com>
2025-07-13 17:01:53 -0400

    Load .env from BASE_DIR; add default SECRET_KEY for build

--- casecrimetrace/settings.py
+++ casecrimetrace/settings.py
@@ -19,13 +19,12 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-environ.Env.read_env()
+env.read_env(os.path.join(BASE_DIR, ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
-
+SECRET_KEY = env("SECRET_KEY", default="django-insecure-collectstatic-only")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG_VALUE") == "True"
 
